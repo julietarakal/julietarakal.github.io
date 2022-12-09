95df7b7210eede80437b8de433564697a9d686ea
Anish PK <anish.kavil@gmail.com>
2022-12-09 18:01:10 +0530

    removed teh key

--- sendemail.sh
+++ sendemail.sh
@@ -3,7 +3,7 @@ echo 'This email will send an email using sendinblue.com'
 curl --request POST \
   --url https://api.sendinblue.com/v3/smtp/email \
   --header 'accept: application/json' \
-  --header 'api-key: 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' \
+  --header 'api-key: <enter key here from site>' \
   --header 'content-type: application/json' \
   --data '{  
    "sender":{  
